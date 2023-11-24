21ff172379576a2abac7580bb63e0b89c1ccea28
VladyslavTodo <124445034+VladyslavTodo@users.noreply.github.com>
2023-11-24 17:28:02 +0200

    hw-7

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,13 +1,13 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
+axios.defaults.baseURL = 'https://656089d483aba11d99d10703.mockapi.io';
+
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
   async (_, thunkAPI) => {
     try {
-      const { data } = await axios.get(
-        'https://656089d483aba11d99d10703.mockapi.io/contacts'
-      );
+      const { data } = await axios.get('/contacts');
       return data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -19,10 +19,7 @@ export const addContact = createAsyncThunk(
   'contacts/addContact',
   async (contact, thunkAPI) => {
     try {
-      const { data } = await axios.post(
-        'https://656089d483aba11d99d10703.mockapi.io/contacts',
-        contact
-      );
+      const { data } = await axios.post('/contacts', contact);
       return data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -34,9 +31,7 @@ export const deleteContact = createAsyncThunk(
   'contacts/deleteContact',
   async (id, thunkAPI) => {
     try {
-      const { data } = await axios.delete(
-        `https://656089d483aba11d99d10703.mockapi.io/contacts/${id}`
-      );
+      const { data } = await axios.delete(`/contacts/${id}`);
       return data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
